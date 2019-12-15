0ff91e86cd7d8826764d56438dd154ae78d7cae9
Palash Bauri <palashbauri1@gmail.com>
2019-12-15 15:37:34 +0530

    added Add Account :rocket:

--- main.py
+++ main.py
@@ -1,8 +1,60 @@
+import tkinter
 from tkinter import *
 from tkinter import font
 from tkinter import ttk
 import pyotp
 import pickledb
+
+class NewAuthtBox(object):
+
+    root = None
+
+    def __init__(self , dict_key):
+        self.top = Toplevel(NewAuthtBox.root)
+        self.top.geometry("500x200")
+        main_frame = Frame(self.top, borderwidth=4, relief='ridge' )
+        main_frame.pack(fill=BOTH, expand=1)
+
+        self.new_font = font.Font(size=21)
+
+        account_label = Label(main_frame, text="Account Name :" , font=self.new_font)
+        account_label.pack(fill = BOTH , expand = 0 , padx=4, pady=4)
+
+        # caller_wants_an_entry = dict_key is not None
+
+        # if caller_wants_an_entry:
+        self.account_entry = Entry(main_frame , font=self.new_font)
+        self.account_entry.pack(fill = X , expand = 0 ,pady=4)
+
+        key_label = Label(main_frame, text="Your Key :", font=self.new_font)
+        key_label.pack(fill = BOTH , expand = 0 ,padx=4, pady=4)
+
+        # caller_wants_an_entry = dict_key is not None
+
+        # if caller_wants_an_entry:
+        self.key_entry = Entry(main_frame, font=self.new_font)
+        self.key_entry.pack(fill = BOTH , expand = 0 ,pady=4)
+
+        b_submit = Button(main_frame, text='Save', font=self.new_font)
+        b_submit['command'] = lambda: self.entry_to_dict(dict_key)
+        b_submit.pack(fill = X , expand = 1 , side = LEFT)
+
+        b_cancel = Button(main_frame, text='Cancel', font=self.new_font)
+        b_cancel['command'] = self.top.destroy
+        b_cancel.pack(fill=X, side=LEFT)
+
+    def entry_to_dict(self, dict_key):
+        account_data = self.account_entry.get()
+        key_data = self.key_entry.get()
+        if account_data and key_data:
+            # d, key = dict_key
+            # d[key] = data
+            # d = {"account" : account_data , "key" : key_data}
+            # d,key = dict_key
+            dict_key["account"] = account_data
+            dict_key['key'] = key_data
+            print(dict_key)
+            self.top.destroy()
 
 
 class Window(Frame):
@@ -11,6 +63,9 @@ class Window(Frame):
         self.master = master
         self.DB = pickledb.load("./toty.db" , True)
         self.DB.set("GIthub (bauripalash)" , "base32secret2332")
+        self.Mbox = NewAuthtBox
+        self.Mbox.root = self.master
+        self.D = {"account" : "" , "key" : ""}
         self.init_window()
 
     def init_window(self):
@@ -39,7 +94,7 @@ class Window(Frame):
 
 
         self.addbtn = Button(self , text="Add +" , font=self.addbtn_font)
-        self.addbtn.config(image = self.add_icon, compound = "left" , command = self.AddNew)
+        self.addbtn.config(image = self.add_icon, compound = "left" , command = lambda: self.AddNew())
         self.addbtn.pack(fill=BOTH , expand=1 ,side=LEFT)
         
         
@@ -61,6 +116,11 @@ class Window(Frame):
 
     def AddNew(self):
         print("Adding New")
+        self.NEWx = self.Mbox(self.D)
+        self.master.wait_window(self.NEWx.top)
+        # print("X" , self.D)
+        self.DB.set(self.D["account"] , self.D["key"])
+        self.authlist.insert(END , self.D["account"])
         
 
     def DelSelected(self):
